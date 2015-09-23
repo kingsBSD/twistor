9a7fc406ed5922d6a85ae44925588bef8beeb3a4
John-David Dalton <john.david.dalton@gmail.com>
2015-09-23 12:40:36 -0700

    Use lodash-fp.

--- etc/follow.js
+++ etc/follow.js
@@ -3,7 +3,7 @@
 const fs = require("fs");
 const path = require("path");
 const Twit = require("twit");
-const R = require("ramda");
+const fp = require("lodash-fp");
 
 const T = new Twit(require("./config.json").tw);
 
@@ -24,29 +24,29 @@ const slugs = [
 
 //FIXME they are however insufficient, we also need @potus and whatever cabinet/mil/intel heads are on twitter
 
-const getUserId = R.map(user => user.id_str);
+const getUserId = fp.map(user => user.id_str);
 
-const getList = R.curry((id,slug) =>
+const getList = fp.curry((id,slug) =>
 	new Promise((y,n) =>
 		T.get("lists/members",
 			{owner_id: id, slug: slug, count: 5000, include_entities: false, skip_status: true},
 			(err,data) => err ? n(err) : y(getUserId(data.users)))));
 
 const follow = id => T.post("friendships/create", {user_id: id}, err => console.log(err || `following ${id}`));
-const followAll = ids => (R.head(ids) && (ids => {
-		follow(R.head(ids));
-		setTimeout(followAll,8*1000,R.tail(ids));
+const followAll = ids => (fp.first(ids) && (ids => {
+		follow(fp.first(ids));
+		setTimeout(followAll,8*1000,fp.rest(ids));
 	})(ids));
 
-const lists = R.map(getList(cspan),slugs);
+const lists = fp.map(getList(cspan),slugs);
 const followings = new Promise((y,n) =>
 	T.get("friends/ids", {user_id: self, count: 5000}, (err,data) => err ? n(err) : y(data.ids)));
 
 const flatList = Promise.all(lists)
-.then(R.pipe(R.flatten,R.uniq))
+.then(fp.flow(fp.flatten,fp.uniq))
 .catch(err => console.log(err));
 
 Promise.all([followings,flatList])
-.then(lists => R.filter(id => R.contains(id,lists[0]),lists[1]))
+.then(lists => fp.filter(id => fp.includes(id,lists[0]),lists[1]))
 .then(followAll)
 .catch(err => console.log(err));

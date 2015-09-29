8bd3e928a1090524fa2b52d39324f31af7f529bb
Alice Maz <alice@alicemaz.com>
2015-09-29 03:51:49 -0400

    oh lol

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
 			options: {
 				sourceMap: true,
 				inSourceMap: "web/static/script.js.map",
-				screwIe8: true
+				screwIE8: true
 			},
 			target: {
 				files: {
